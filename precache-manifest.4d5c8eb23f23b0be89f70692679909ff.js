f30e89cb1954ef663efaee5d564e74fa6211e9e7
thibault_judicq <thibault.judicq@gmail.com>
2020-08-30 22:32:14 +0800

    Updates

---
+++ precache-manifest.4d5c8eb23f23b0be89f70692679909ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7782ddfab3dd115b4a606827efe9a984",
+    "url": "/lambdaweb/index.html"
+  },
+  {
+    "revision": "5f065d1679fb4bdcba30",
+    "url": "/lambdaweb/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c09f004ffdf7ee1b23bd",
+    "url": "/lambdaweb/static/js/2.62e28217.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lambdaweb/static/js/2.62e28217.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f065d1679fb4bdcba30",
+    "url": "/lambdaweb/static/js/main.cf7725c6.chunk.js"
+  },
+  {
+    "revision": "ace8e64e4238cf05bd00",
+    "url": "/lambdaweb/static/js/runtime-main.df365093.js"
+  }
+]);
